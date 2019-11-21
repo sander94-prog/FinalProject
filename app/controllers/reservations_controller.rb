5d99a6fd800d721b08bc30150048d8e5cea485dc
Sadie Anderson <sander94@uncc.edu>
2019-11-21 16:28:40 +0000

    Complete Create and SHow

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,20 @@
 class ReservationsController < ApplicationController
+    def show
+        @reservation = Reservation.find(params[:id])
+    end
+    
     def new
     end
+    
+    def create
+        @reservation = Reservation.new(reservation_params)
+        
+        @reservation.save
+        redirect_to @reservation
+    end
+end
+
+private
+def reservation_params
+    params.require(:reservation).permit(:lot, :license, :make, :model)
 end
