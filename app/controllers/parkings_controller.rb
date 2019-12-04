977642f3b5641509be190e0fac404557920de957
Sadie Anderson <sander94@uncc.edu>
2019-12-04 21:57:41 +0000

    Buisness and Customer done

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -1,8 +1,49 @@
 class ParkingsController < ApplicationController
+    def index
+        @parking = Parking.all
+    end 
+    
+    def show
+        @parking = Parking.find(params[:id])
+    end
+    
     def new
+        @parking = Parking.new
     end
     
     def create
-        render plain: params[:parking]
+        @parking = Parking.new(parking_params)
+        
+        if @parking.save
+            redirect_to @parking
+        else
+            render 'new'
+        end
     end
+    
+    def edit
+        @parking = Parking.find(params[:id])
+    end
+    
+    def update
+        @parking = Parking.find(params[:id]) 
+        
+        if @parking.update(parking_params)
+            redirect_to @parking
+        else
+            render 'edit' 
+        end
+    end
+    
+    def destroy
+        @parking = Parking.find(params[:id])
+        @parking.destroy
+        
+        redirect_to parkings_path
+    end
+end
+
+private
+def parking_params
+    params.require(:parking).permit(:name, :address)
 end
--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,2 +1,4 @@
 class Parking < ApplicationRecord
+     validates :name, presence: true, length: {minimum: 4}
+     validates :address, presence: true, length: {minimum: 7}
 end
