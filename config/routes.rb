ee4a15d2d208f530ea51c46d259c78f12ae02c38
Di Ta <dta1@uncc.edu>
2019-12-09 20:54:35 +0000

    Finalize google sign in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy'
+  
+  resources :sessions, only: [:create, :destroy]
   get 'welcome/index'
   
   resources :reservations, :parkings, :spots
