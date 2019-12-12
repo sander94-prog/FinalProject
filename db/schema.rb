1ce8907ae3013646e8c762dc13fb0e31341247ba
Di Ta <dta1@uncc.edu>
2019-12-12 01:30:14 +0000

    resize qr code

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20191209203248) do
 
-  create_table "buisnesses", force: :cascade do |t|
-    t.text     "name"
-    t.text     "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "parkings", force: :cascade do |t|
     t.string   "name"
     t.text     "address"
