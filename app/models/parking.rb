9990a23e6e97f9b8e380416f43c06c07eab0d46e
Sadie Anderson <sander94@uncc.edu>
2019-12-12 01:19:28 +0000

    Added QR COde

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,4 +1,4 @@
 class Parking < ApplicationRecord
-     validates :name, presence: true, length: {minimum: 4}
-     validates :address, presence: true, length: {minimum: 7}
+     validates :name, presence: true, length: {minimum: 2}
+     validates :address, presence: true, length: {minimum: 5}
 end
